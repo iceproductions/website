94086d9f5c3566830cc1362facb02b8000f0885e
Daniel Bulant <admin@danbulant.eu>
2020-04-01 15:03:50 +0200

    Define basic data connections

--- scripts/lib/bot.js
+++ scripts/lib/bot.js
@@ -1,3 +1,33 @@
 class BotConnector {
-    base = "https://api.iceproductions.dev/v1"
+    base = "https://api.iceproductions.dev/v1";
+
+    request({
+        method = "POST",
+        type = "json",
+        longType = "application/json",
+        body,
+        endpoint = this.base
+    }) {
+        return new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest();
+            xhr.responseType = type;
+            xhr.open(method, endpoint);
+            xhr.setRequestHeader('Content-Type', longType);
+            xhr.onload = function () {
+                if(xhr.status == 200){
+                    resolve(xhr.response);
+                } else {
+                    reject(xhr.response);
+                }
+            };
+
+            xhr.send(body);
+        });
+    }
+
+    async getData(schema){
+        return await this.request({
+            body: schema
+        });
+    }
 }
