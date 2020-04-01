dc0f6744d28a32a4a70f95ffd707435c7cbefff4
Daniel Bulant <admin@danbulant.eu>
2020-04-01 15:20:13 +0200

    Implement caching and individual queries

--- scripts/lib/bot.js
+++ scripts/lib/bot.js
@@ -39,7 +39,9 @@ class BotConnector {
 
 
     async all(){
-        return await this.getData(`
+        if(!this._all)
+        if(!this._info && !this._commands)
+        this._all = this.getData(`
         {
             info {
                 repository,
@@ -68,5 +70,80 @@ class BotConnector {
             }
         }
         `);
+        this._info = this._all.info;
+        this._commands = this._all.commands;
+        return this._all;
+    }
+
+    async info(){
+        if(!this._info)
+        this._info = await this.getData(`
+        {
+            info {
+                repository,
+                status,
+                version {
+                    string,
+                    major,
+                    minor,
+                    patch,
+                    channel
+                }
+            }
+        }
+        `);
+        
+        return this._info;
+    }
+
+    async commands(){
+        if(!this._commands){
+            this._commands = await this.getData(`
+            {
+                commands {
+                    name,
+                    group,
+                    description,
+                    aliases,
+                    usage,
+                    arguments {
+                        type,
+                        prompt,
+                        default,
+                        key,
+                        infinite
+                    }
+                }
+            }
+            `);
+        }
+        return this._commands;
+    }
+
+    async command(name){
+        if(this._commands){
+            return this._commands.filter(c => c.name === name)[0];
+        }
+        if(!this._command[name])
+        this._command[name] = await this.getData(`
+        {
+            command(name: "${name}"){
+                name,
+                group,
+                description,
+                aliases,
+                usage,
+                arguments {
+                    type,
+                    prompt,
+                    default,
+                    key,
+                    infinite
+                }
+            }
+        }
+        `);
+
+        return this._command[name];        
     }
 }
