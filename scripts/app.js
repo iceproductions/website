ba852193ecd5c80b31f962b976fd456350d87bb8
marcosnasa <marcosgnasa@gmail.com>
2020-06-05 22:17:03 +0200

    Nav-bar (mobile).

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,38 @@
-(async () => {
-    const data = new BotConnector;
+"use strict";
+
+var app = (function App() {
+  const data = new BotConnector();
+
+  return { init };
+
+  function init() {
+    initNavigationMenu();
+    // renderCommandTable();
+  }
+
+  function initNavigationMenu() {
+    var navMenuList = document.getElementById("nav-menu");
+    var hamburgerButton = document.getElementById("hamburger-icon");
+    hamburgerButton.addEventListener("click", () => {
+      navMenuList.classList.toggle("displayed");
+      hamburgerButton.classList.toggle("clicked");
+      navMenuList.animate([{ opacity: 0 }, { opacity: 1 }], { duration: 300 });
+    });
+  }
+
+  async function renderCommandTable() {
     const json = await data.commands();
     const table = document.getElementById("commands");
-    json.commands.forEach(cmd => {
-        table.innerHTML += `
-            <tr>
-                <td>${cmd.group}</td>
-                <td>${cmd.name}</td>
-                <td>${cmd.description}</td>
-            </tr>
-        `;
+    json.commands.forEach((cmd) => {
+      table.innerHTML += `
+              <tr>
+                  <td>${cmd.group}</td>
+                  <td>${cmd.name}</td>
+                  <td>${cmd.description}</td>
+              </tr>
+          `;
     });
+  }
 })();
+
+app.init();
